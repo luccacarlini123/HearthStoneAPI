5a10c9b556408f8f8982de2d2827042f13dbfd93
luccacarlini123 <31741340+luccacarlini123@users.noreply.github.com>
2021-02-02 15:31:18 +0300

    Delete HearthStone_SpringRest/exception directory

--- HearthStone_SpringRest/exception/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mouzetech.exception;
-
-public class IdNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public IdNotFoundException(String msg) {
-		super(msg);
-	}
-}
